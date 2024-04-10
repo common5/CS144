39b32b6fb6b7ea21134e01771d6ee36683c55a84
common5 <1329361431@qq.com>
2024-04-10 10:24:34 +0800

    checkpoint2 completed

--- src/tcp_receiver.cc
+++ src/tcp_receiver.cc
@@ -5,11 +5,39 @@ using namespace std;
 void TCPReceiver::receive( TCPSenderMessage message )
 {
   // Your code here.
-  (void)message;
+  // (void)message;
+  if(message.RST)
+  {
+    reassembler_.reader().set_error();
+    return;
+  }
+  const uint64_t checkpoint = reassembler_.writer().bytes_pushed();// expected stream index
+  if(!ISN_.has_value())
+  {
+    if(message.SYN)
+      ISN_ = message.seqno;
+    else
+      return;
+  }
+  else
+  {
+    if(checkpoint <= (static_cast<uint64_t>(1) << 32) && message.seqno == *ISN_) // 当且仅当checkpoint <= 2^32的时候, message.seqno才可能等于ISN, 即非法
+    {
+      return;
+    }
+  }
+  reassembler_.insert(message.seqno.unwrap(*ISN_, checkpoint) - (*ISN_ != message.seqno), std::move(message.payload), message.FIN);
 }
 
 TCPReceiverMessage TCPReceiver::send() const
 {
   // Your code here.
-  return {};
+  // return {};
+  std::optional<Wrap32> seqno = std::nullopt;
+  if(ISN_.has_value())
+  {
+    // getchar();
+    seqno = Wrap32::wrap(reassembler_.writer().bytes_pushed(), *ISN_) + (1 + reassembler_.writer().is_closed());
+  }
+  return { seqno, static_cast<uint16_t>(min(reassembler_.writer().available_capacity(), static_cast<uint64_t>(65535))), reassembler_.writer().has_error() };
 }
--- src/wrapping_integers.cc
+++ src/wrapping_integers.cc
@@ -5,15 +5,22 @@ using namespace std;
 Wrap32 Wrap32::wrap( uint64_t n, Wrap32 zero_point )
 {
   // Your code here.
-  (void)n;
-  (void)zero_point;
-  return Wrap32 { 0 };
+  // (void)n;
+  // (void)zero_point;
+  return zero_point + (n & 0xffffffff);
 }
 
 uint64_t Wrap32::unwrap( Wrap32 zero_point, uint64_t checkpoint ) const
 {
   // Your code here.
-  (void)zero_point;
-  (void)checkpoint;
-  return {};
+  // (void)zero_point;
+  // (void)checkpoint;
+  // return {};
+  const uint64_t mod = static_cast<uint64_t>(1) << 32;
+  uint32_t dist = raw_value_ - wrap(checkpoint, zero_point).raw_value_;
+  if(dist <= (static_cast<uint32_t>(1) << 31) || checkpoint + dist < mod)
+  {
+    return dist + checkpoint;
+  }
+  return dist + checkpoint - mod;
 }
